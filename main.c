cf2d4c96a8362a943c8c19fedbde1956afb22ff3
ylitsak <saqque91pl@gmail.com>
2024-11-28 12:34:10 +0200

    Makefile setup and starting to work on project now

---
+++ main.c
@@ -0,0 +1,22 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   main.c                                             :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: saylital <saylital@student.hive.fi>        +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/11/28 12:27:11 by saylital          #+#    #+#             */
+/*   Updated: 2024/11/28 12:31:22 by saylital         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "minishell.h"
+
+int	main(int argc, char *argv[], char *envp[])
+{
+	(void)argc;
+	(void)argv;
+	(void)envp;
+	printf("Hello World\n");
+	return (0);
+}
